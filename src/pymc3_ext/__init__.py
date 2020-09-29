8dbe0ea74748be01cdb096a0ec33cf3c37d06fa6
Dan F-M <danfm@nyu.edu>
2020-09-29 14:51:09 -0400

    fixing import order

--- src/pymc3_ext/__init__.py
+++ src/pymc3_ext/__init__.py
@@ -9,10 +9,10 @@ __all__ = [
 ]
 
 from . import optim, sampling
+from .distributions import *  # noqa
 from .optim import optimize
 from .pymc3_ext_version import __version__
 from .sampling import sample
-from .distributions import *  # noqa
 
 __uri__ = "https://github.com/exoplanet-dev/pymc3-ext"
 __author__ = "Daniel Foreman-Mackey"
