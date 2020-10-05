18b4df5cc5118b88dbdbb549cb7b42a58c9216ae
Dan F-M <foreman.mackey@gmail.com>
2020-10-05 15:52:12 -0400

    import utils

--- src/pymc3_ext/__init__.py
+++ src/pymc3_ext/__init__.py
@@ -7,12 +7,22 @@ __all__ = [
     "optimize",
     "__version__",
     "sample",
+    "eval_in_model",
+    "get_samples_from_trace",
+    "get_args_for_theano_function",
+    "get_theano_function_for_var",
 ]
 
 from . import distributions, optim, sampling
 from .optim import optimize
 from .pymc3_ext_version import __version__
 from .sampling import sample
+from .utils import (
+    eval_in_model,
+    get_args_for_theano_function,
+    get_samples_from_trace,
+    get_theano_function_for_var,
+)
 
 __uri__ = "https://docs.exoplanet.codes"
 __author__ = "Daniel Foreman-Mackey"
