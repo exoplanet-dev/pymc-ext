e3ba0c858dcf7607942205bd4ce11b0387d6857e
Dan F-M <foreman.mackey@gmail.com>
2022-10-20 12:03:27 -0400

    handling strange choices by pymc-devs

--- src/pymc_ext/optim.py
+++ src/pymc_ext/optim.py
@@ -1,12 +1,24 @@
 __all__ = ["optimize"]
 
 import pymc as pm
+from aesara.graph.basic import graph_inputs
+from aesara.tensor.var import TensorConstant, TensorVariable
 
 
 def optimize(start=None, vars=None, **kwargs):
-    """A thin wrapper around ``pymc.find_MAP`` that can handle deterministics"""
+    """A thin wrapper around ``pymc.find_MAP`` that actually does something sensible :D"""
     if vars is not None:
         if not isinstance(vars, (list, tuple)):
             vars = [vars]
+
+        # find_MAP only supports passing in members of free_RVs, so let's deal
+        # with that here...
         vars = pm.aesaraf.rvs_to_value_vars(vars)
+        vars = [
+            v
+            for v in graph_inputs(vars)
+            if isinstance(v, TensorVariable)
+            and not isinstance(v, TensorConstant)
+        ]
+
     return pm.find_MAP(start=start, vars=vars, **kwargs)
