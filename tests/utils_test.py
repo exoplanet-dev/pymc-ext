d0844764c25d854c87ebb888b073c4938af44181
Thomas Vandal <thomas.vandal@umontreal.ca>
2023-10-31 20:58:40 -0400

    Update testval to initval in utils_test.py
    testval is deprecated now

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -9,6 +9,6 @@ def test_eval_in_model(seed=123409):
     x_val = np.random.randn(5, 3)
     x_val2 = np.random.randn(5, 3)
     with pm.Model():
-        x = pm.Normal("x", shape=x_val.shape, testval=x_val)
+        x = pm.Normal("x", shape=x_val.shape, initval=x_val)
         assert np.allclose(eval_in_model(x), x_val)
         assert np.allclose(eval_in_model(x, {"x": x_val2}), x_val2)
